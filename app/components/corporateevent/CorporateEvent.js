bbda71754433dab96feb3d21a2b0f94ccc4b096a
irfan-Ghyan <irfan@teleios.ae>
2024-09-30 10:18:08 +0400

    updated

--- app/components/corporateevent/CorporateEvent.js
+++ app/components/corporateevent/CorporateEvent.js
@@ -39,14 +39,14 @@ const DomeData = () => {
       { title: 'CORPORATE EVENTS', description: 'The Teleios Dome is a unique corporate event venue combining luxurious surroundings with state-of-the-art technology. Our stunning open-plan Reception and Lounge area is complemented by a Cafeteria and Private Conference Room. You and your guests can then step into our spacious Gaming Room and experience exhilarating simulator racing. For the select few, enjoy the exclusivity of our VIP room.   A blend of advanced technology with refined surroundings, book The Teleios Dome for your next Corporate Event.' },
     ];
   return (
-    <div className="relative w-full max-w-full overflow-hidden h-[700px] xl:h-[785px] bg-cover bg-center px-4 md:px-0" style={{ backgroundImage: "url('/assets/images/experience/corbg.jpg')" }}>
- <div className="absolute  inset-0 px-4 md:flex md:flex-col md:pr-6 py-[40px] lg:py-[80px] xl:py-[80px] max-w-7xl mx-auto">
+    <div className="relative w-full max-w-full overflow-hidden h-[850px] sm:h-[600px] md:h-[600px] lg:h-[700px] xl:h-[785px] bg-cover bg-right lg:bg-center px-4 md:px-0 " style={{ backgroundImage: "url('/assets/images/experience/corbg.jpg')" }}>
+ <div className="bg-[#11072C] bg-opacity-60 lg:bg-opacity-0 md:bg-opacity-0 xl:bg-opacity-0 px-4 md:flex md:flex-col md:pr-6  py-[20px] lg:py-[80px] xl:py-[80px] max-w-7xl mx-auto mt-[200px] sm:mt-0 md:mt-0 lg:mt-0 xl:mt-0 ">
    <div className='px-4 flex flex-col justify-between md:mt-[36px] md:mb-[41px]'>
      {domes.map((dome, index) => (
        <div key={index} className='py-[15px] lg:py-[50px] md:w-[400px] lg:w-[700px] xl:w-[700px] '>
            {/* <h4 className='text-[34px] xl:text-[35px] text-[#D008A6] font-bold font-jura drop-shadow-4xl'>{dome.subtitle}</h4> */}
          <h1 className='text-[34px] md:text-[54px] text-white font-black font-orbitron drop-shadow-4xl'>{dome.title}</h1>
-         <p className='md:text-[14px] lg:text-[18px] text-white font-black font-jura mt-6 text-balance drop-shadow-4xl leading-7 lg:leading-10 xl:leading-10 text-justify'>{dome.description}</p>
+         <p className='md:text-[14px] lg:text-[18px] text-white font-black font-jura mt-6 text-balance drop-shadow-4xl lg:leading-10 xl:leading-10 text-justify'>{dome.description}</p>
        </div>
      ))}
      {/* <div className="flex items-start m-bottom">
